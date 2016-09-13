d7f0decc987f2d04a6b2a8cb6a2ac9a654e308c1
Scott Nath <snath@us.ibm.com>
2016-09-13 11:04:28 -0400

    :white_check_mark: :new: new checking with tests

--- tests/validation.js
+++ tests/validation.js
@@ -22,10 +22,24 @@ const badInput = {
   },
 };
 
+const decimal = {
+  target: {
+    name: 'decimal',
+    value: 1.00003,
+  },
+};
+
+const floater = {
+  target: {
+    name: 'floater',
+    value: '000001.00003',
+  },
+};
+
 
 // Valid input
 test('valid input', t => {
-  t.true(validation(input), 'Valid input returns true');
+  t.true(validation(input), 'Valid number input returns true');
 });
 
 // Valid string number
@@ -33,8 +47,17 @@ test('valid string', t => {
   t.true(validation(string), 'Valid number as string returns true');
 });
 
+// Valid bad input returns error
+test('validate number with decim is a number', t => {
+  t.is(validation(badInput), 'number must be a number!', 'Return string if not valid');
+});
 
 // Valid bad input returns error
-test('validate input is a number', t => {
-  t.is(validation(badInput), 'number must be a number!', 'Return string if not valid');
+test('validate number with decimals is a number', t => {
+  t.true(validation(decimal), 'Valid decimal input returns true');
+});
+
+// Valid bad input returns error
+test('validate number with leading zeros is a number', t => {
+  t.true(validation(floater), 'Valid floating input returns true');
 });
